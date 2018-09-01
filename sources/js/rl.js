0bd2d0ffe40fcd0b8d8920d6d69308f303b34bbc
bjoern-hempel <bjoern@hempel.li>
2018-09-02 00:20:16 +0200

    add shortcut do define the environment

--- sources/js/rl.js
+++ sources/js/rl.js
@@ -1,4 +1,194 @@
 /**
+ * The class that represents a state.
+ *
+ * @author  Björn Hempel <bjoern@hempel.li>
+ * @version 1.0 (2018-09-01)
+ */
+class State {
+
+    /**
+     * The constructor of this class.
+     *
+     * @param idState
+     */
+    constructor(idState) {
+        this.ids = {
+            idState: idState
+        };
+    }
+
+    /**
+     * Returns the state id.
+     *
+     * @returns Integer
+     */
+    get idState() {
+        return this.ids.idState;
+    }
+}
+
+/**
+ * The class that represents an action.
+ *
+ * @author  Björn Hempel <bjoern@hempel.li>
+ * @version 1.0 (2018-09-01)
+ */
+class Action {
+
+    /**
+     * The constructor of this class.
+     *
+     * @param state
+     * @param idAction
+     */
+    constructor(state, idAction) {
+        this.state = state;
+
+        this.ids = {
+            idAction: idAction
+        };
+    }
+
+    /**
+     * Returns the state id.
+     *
+     * @returns Integer
+     */
+    get idState() {
+        return this.state.idState;
+    }
+
+    /**
+     * Returns the action id.
+     *
+     * @returns Integer
+     */
+    get idAction() {
+        return this.ids.idAction;
+    }
+}
+
+/**
+ * The class that represents a state change.
+ *
+ * @author  Björn Hempel <bjoern@hempel.li>
+ * @version 1.0 (2018-09-01)
+ */
+class StateChange {
+
+    /**
+     * The constructor of this class.
+     *
+     */
+    constructor() {
+        this.state       = null;
+        this.action      = null;
+        this.stateTarget = null;
+
+        this.stateTargetFirst = true;
+
+        this.values = {
+            likelihood: null,
+            reward:     null
+        };
+
+        /* Iterate through all arguments. */
+        for (var i = 0; i < arguments.length; i++) {
+            switch(true) {
+                /* object State given */
+                case arguments[i] instanceof State:
+                    if (this.stateTargetFirst) {
+                        if (this.stateTarget === null) {
+                            this.stateTarget = arguments[i];
+                        } else if (this.state === null) {
+                            this.state = arguments[i];
+                        } else {
+                            console.error('Too much state objects given.');
+                        }
+                    } else {
+                        if (this.state === null) {
+                            this.state = arguments[i];
+                        } else if (this.stateTarget === null) {
+                            this.stateTarget = arguments[i];
+                        } else {
+                            console.error('Too much state objects given.');
+                        }
+                    }
+
+                    this.stateTarget = arguments[i];
+                    break;
+
+                /* object Action given */
+                case arguments[i] instanceof Action:
+                    this.action = arguments[i];
+                    break;
+
+                /* number given (likelihood and reward) */
+                case typeof arguments[i] == 'number' && !isNaN(arguments[i]) && isFinite(arguments[i]):
+                    if (this.values.likelihood === null) {
+                        this.values.likelihood = arguments[i];
+                    } else if (this.values.reward === null) {
+                        this.values.reward = arguments[i];
+                    } else {
+                        console.error('Too much numbers given!');
+                        continue;
+                    }
+                    break;
+            }
+        }
+
+
+    }
+
+    /**
+     * Returns the state id.
+     *
+     * @returns Integer
+     */
+    get idState() {
+        return this.state !== null ? this.state.idState : null;
+    }
+
+    /**
+     * Returns the action id.
+     *
+     * @returns Integer
+     */
+    get idAction() {
+        return this.action.idAction;
+    }
+
+    /**
+     * Returns the id from state target.
+     *
+     * @returns Integer
+     */
+    get idStateTarget() {
+        console.log(this.stateTarget);
+
+        return this.stateTarget.idState;
+    }
+
+    /**
+     * Returns the id from state target.
+     *
+     * @returns Integer
+     */
+    get likelihood() {
+        return this.values.likelihood;
+    }
+
+    /**
+     * Returns the id from state target.
+     *
+     * @returns Integer
+     */
+    get reward() {
+        return this.values.reward;
+    }
+}
+
+/**
  * A class to do some reinforcement learning stuff.
  *
  * @author  Björn Hempel <bjoern@hempel.li>
@@ -34,18 +224,52 @@ class ReinforcementLearning {
     addState() {
         this.statesActionsStatesTR.push([]);
 
-        return this.statesActionsStatesTR.length - 1;
+        return new State(this.statesActionsStatesTR.length - 1);
     }
 
     /**
      * Adds a new action to this environment.
      *
-     * @param state
      */
-    addAction(state) {
-        this.statesActionsStatesTR[state].push({});
+    addAction() {
+        var action = null;
 
-        return [state, this.statesActionsStatesTR[state].length - 1];
+        /* Iterate through all arguments. */
+        for (var i = 0; i < arguments.length; i++) {
+            switch(true) {
+                case arguments[i] instanceof State:
+                    var state = arguments[i];
+
+                    /* Multiple entries of the state object. */
+                    if (action !== null) {
+                        console.error('Only one state is allowed!');
+                        continue;
+                    }
+
+                    this.statesActionsStatesTR[state.idState].push({});
+                    action = new Action(state, this.statesActionsStatesTR[state.idState].length - 1);
+                    break;
+            }
+        }
+
+        /* No state was given */
+        if (action === null) {
+            console.error('No State was given!');
+            return null;
+        }
+
+        /* Iterate through all arguments. */
+        for (var i = 0; i < arguments.length; i++) {
+            switch(true) {
+                case arguments[i] instanceof StateChange:
+                    var sc = arguments[i];
+
+                    this.addStateChange(action, sc.stateTarget, sc.likelihood, sc.reward);
+                    break;
+            }
+        }
+
+        return action;
     }
 
     /**
@@ -56,7 +280,7 @@ class ReinforcementLearning {
      * @param reward
      */
     addStateChange(action, toState, likelihood, reward) {
-        this.statesActionsStatesTR[action[0]][action[1]][toState] = [likelihood, reward];
+        this.statesActionsStatesTR[action.idState][action.idAction][toState.idState] = [likelihood, reward];
     }
 
     /**
--- tests/js/rl.js
+++ tests/js/rl.js
@@ -1,6 +1,6 @@
 function startRLTest() {
 
-    /* RL: Calculate Q test */
+    /* RL: Calculate Q test 1 */
     new JsSuccessTest(
         ReinforcementLearning.SUCCESS_CALCULATE_Q,
         new JsTestTestFunction(function () {
@@ -30,6 +30,40 @@ function startRLTest() {
             rl.addStateChange(a5, s0, 0.8,  40);
             rl.addStateChange(a5, s1, 0.1,   0);
             rl.addStateChange(a5, s2, 0.1,   0);
+
+            var Q = rl.calulateQ(iterations, discountRate);
+
+            var QExpected = [
+                [21.8864, 20.7914, 16.8548],
+                [ 1.1080,  1.1670],
+                [53.8607]
+            ];
+
+            return (
+                JsTest.equalArrayValues(Q, QExpected, 3)
+            );
+        })
+    );
+
+    /* RL: Calculate Q test 2 */
+    new JsSuccessTest(
+        ReinforcementLearning.SUCCESS_CALCULATE_Q,
+        new JsTestTestFunction(function () {
+            var discountRate = 0.95;
+            var iterations   = 100;
+
+            var rl = new ReinforcementLearning();
+
+            var s0 = rl.addState();
+            var s1 = rl.addState();
+            var s2 = rl.addState();
+
+            rl.addAction(s0, new StateChange(s0, 0.7,  10), new StateChange(s1, 0.3, 0));
+            rl.addAction(s0, new StateChange(s0, 1.0,   0));
+            rl.addAction(s0, new StateChange(s0, 0.8,   0), new StateChange(s1, 0.2, 0));
+            rl.addAction(s1, new StateChange(s1, 1.0,   0));
+            rl.addAction(s1, new StateChange(s2, 1.0, -50));
+            rl.addAction(s2, new StateChange(s0, 0.8,  40), new StateChange(s1, 0.1, 0), new StateChange(s2, 0.1, 0));
 
             var Q = rl.calulateQ(iterations, discountRate);
 
